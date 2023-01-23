2f672ad707e9171db27fc8b29e1f378d77029f35
Abdelrahman Ehab <abdu.ehab0@gmail.com>
2023-01-23 22:43:31 +0200

    added quick bench's example

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,19 +2,28 @@
 
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
+static void StringCreation(benchmark::State &state)
+{
+  // Code inside this loop is measured repeatedly
   for (auto _ : state)
-    std::string empty_string;
+  {
+    std::string created_string("hello");
+    // Make sure the variable is not optimized away by compiler
+    benchmark::DoNotOptimize(created_string);
+  }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(StringCreation);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
+static void StringCopy(benchmark::State &state)
+{
+  // Code before the loop is not measured
   std::string x = "hello";
   for (auto _ : state)
+  {
     std::string copy(x);
+  }
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(StringCopy);
 
 BENCHMARK_MAIN();
